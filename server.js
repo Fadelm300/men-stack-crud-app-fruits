fcbfd0fb2d8c5d25cb2937e07700d5179878d1db
Fadem300 <fadel.m200@gmail.com>
2024-07-21 14:29:38 +0300

    conact the MongoDB

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 // Here is where we import modules
 // We begin by loading Express
-const express = require("express");
 
-const app = express();
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
 
-app.listen(3001, () => {
-  console.log("Listening on port 3001");
+// DATABASE
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 
 
-// // server.js
+const app = express();
 
-// // GET /
-// app.get("/", async (req, res) => {
-//     res.send("hello, friend!");
-//   });
+// MIDDLEWARE
+app.use(morgan('dev'));
 
+// ROUTES
 
+// Landing Page
+app.get('/', (req, res, next) => {
+  res.render('index.ejs');
+});
 
-// server.js
-
-// GET /
-app.get("/", async (req, res) => {
-    res.render("index.ejs");
-  });
-
-
-  
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
